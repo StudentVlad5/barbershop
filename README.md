288e236dd1a178c9a6b2e31092118f3180281059
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-17 16:05:39 +0000

    Deploying to gh-pages from @ StudentVlad5/barbershop@f1daf560421b6f63180c3d96f44592de1785ca15 🚀

--- README.md
+++
@@ -1,7 +0,0 @@
-# Project Pet Support
-
-Start project for the barbershops
-## Developers
-
-- [Julia Golban](https://github.com/JuliaGolban)
-- [Vladislav Popov](https://github.com/StudentVlad5)
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { HeaderContainer } from './Header.styled';
-
-export const Header = () => {
-  return (
-    <HeaderContainer>
-      <h1>HEllO WORLD</h1>
-    </HeaderContainer>
-  );
-};
