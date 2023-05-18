acc9b744390baa38049b61c740b6a4ad18b9f4a3
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-18 16:33:00 +0300

    shadule with members

--- src/components/Schedule/Datasource.js
+++ src/components/Schedule/Datasource.js
@@ -1,28 +1,36 @@
 export const data = [
-    {
-      Id: 1,
-      Subject: 'Meeting - 1',
-      StartTime: new Date(2023, 6, 18, 10, 0),
-      EndTime: new Date(2023, 6, 18, 11, 30),
-      IsAllDay: false,
-      Status: 'Completed',
-      Priority: 'High'
-    },
-    {
-      Id: 2,
-      Subject: 'Meeting - 2',
-      StartTime: new Date(2023, 6, 19, 13, 0),
-      EndTime: new Date(2023, 6, 19, 14, 0),
-      IsAllDay: false,
-      Status: 'Completed',
-      Priority: 'High'
+  {
+    Id: 1,
+    Subject: "Meeting - 1",
+    StartTime: new Date(2023, 4, 18, 10, 0),
+    EndTime: new Date(2023, 4, 18, 11, 30),
+    IsAllDay: false,
+    Status: "Completed",
+    Priority: "High",
+    OwnerId: 1,
   },
-  ];
+  {
+    Id: 2,
+    Subject: "Meeting - 2",
+    StartTime: new Date(2023, 4, 19, 13, 0),
+    EndTime: new Date(2023, 4, 19, 14, 0),
+    IsAllDay: false,
+    Status: "Completed",
+    Priority: "High",
+    OwnerId: 2,
+  },
+];
 
-  export const fieldsData = {
-    id: 'Id',
-    subject: { name: 'Subject' },
-    isAllDay: { name: 'IsAllDay' },
-    startTime: { name: 'StartTime' },
-    endTime: { name: 'EndTime' }
-}
+export const fieldsData = {
+  id: "Id",
+  subject: { name: "Subject" },
+  isAllDay: { name: "IsAllDay" },
+  startTime: { name: "StartTime" },
+  endTime: { name: "EndTime" },
+};
+
+export const ownersData = [
+  { OwnerText: "Julia", Id: 1, OwnerColor: "#ffaa00", designation: 'Cardioligst' },
+  { OwnerText: "Grogoriy", Id: 2, OwnerColor: "#f8a398", designation: 'Neurologist' },
+  { OwnerText: "Vlad", Id: 3, OwnerColor: "#7499e1", designation: 'Orthopedic Surgeon' },
+];
--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -12,36 +12,79 @@ import {
   Inject,
   ViewsDirective,
   ViewDirective,
+  ResourcesDirective,
+  ResourceDirective,
 } from "@syncfusion/ej2-react-schedule";
-import { data, fieldsData } from "./Datasource";
+import { data, fieldsData, ownersData } from "./Datasource";
 import { extend } from "@syncfusion/ej2-base";
 
 const Schedule = () => {
+  function getDoctorName(value) {
+    return value.resourceData
+      ? value.resourceData[value.resource.textField]
+      : value.resourceName;
+  }
+  function getDoctorLevel(value) {
+    let resourceName = getDoctorName(value);
+    return resourceName === "Julia"
+      ? "Cardiologist"
+      : resourceName === "Grogoriy"
+      ? "Neurologist"
+      : "Orthopedic Surgeon";
+  }
+
   const dates = extend([], data, null, true);
+  const [ownerData] = React.useState(ownersData);
+  const group = { resources: ["Doctors"] };
   const eventSettings = { dataSource: dates, fields: fieldsData };
   // const workingDays = [1, 3, 5];
   const today = new Date();
 
+  function resourceHeaderTemplate(props) {
+    return (
+      <div className="template-wrap">
+        <div className="resource-detail">
+          <div className="resource-name">{getDoctorName(props)}</div>
+          <div className="resource-designation">{getDoctorLevel(props)}</div>
+        </div>
+      </div>
+    );
+  }
   return (
     <ScheduleComponent
       height="550px"
       width="100%"
-      currentView="Week"
+      currentView="WorkWeek"
       selectedDate={
         new Date(
           today.getFullYear().toString(),
-          (today.getMonth() + 2).toString(),
+          today.getMonth().toString(),
           today.getDate().toString()
         )
       }
+      resourceHeaderTemplate={resourceHeaderTemplate}
       eventSettings={eventSettings}
+      group={group}
     >
+      <ResourcesDirective>
+        <ResourceDirective
+          field="OwnerId"
+          title="Subject"
+          name="Doctors"
+          dataSource={ownerData}
+          textField="OwnerText"
+          idField="Id"
+          DesignationField="designation"
+          colorField="OwnerColor"
+        ></ResourceDirective>
+      </ResourcesDirective>
       <ViewsDirective>
-        {/* <ViewDirective option="Day" /> */}
+        <ViewDirective option="Day" />
         <ViewDirective option="Week" startHour="10:00" endHour="18:00" />
         <ViewDirective option="WorkWeek" startHour="7:00" endHour="21:00" />
         <ViewDirective option="Month" showWeekend={false} />
       </ViewsDirective>
+
       <Inject
         services={[
           Day,
