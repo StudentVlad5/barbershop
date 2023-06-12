733b40992a6d65431a8dd2c1d4439b93101b0317
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-12 11:00:55 +0300

    add no-scroll for body

--- src/hooks/modalWindow.js
+++ src/hooks/modalWindow.js
@@ -9,24 +9,24 @@ export function openModalWindow(e) {
   e.preventDefault();
   document.querySelector("#popup-root").classList.remove("is-hide");
   window.addEventListener("keydown", closeByEsc);
-  document.querySelector("body").classList.add("scroll");
+  document.querySelector("body").style.overflow = "hidden";
 }
 
 export function closeModalWindow() {
   document.querySelector("#popup-root").classList.add("is-hide");
   window.removeEventListener("keydown", closeByEsc);
-  document.querySelector("body").classList.remove("scroll");
+  document.querySelector("body").style.overflow = "visible";
 }
 
 export function openModalForm(e) {
   e.preventDefault();
   document.querySelector("#popup-register-root").classList.remove("is-hide");
   window.addEventListener("keydown", closeByEsc);
-  document.querySelector("body").classList.add("scroll");
+  document.querySelector("body").style.overflow = "hidden";
 }
 
 export function closeModalForm() {
   document.querySelector("#popup-register-root").classList.add("is-hide");
   window.removeEventListener("keydown", closeByEsc);
-  document.querySelector("body").classList.remove("scroll");
+  document.querySelector("body").style.overflow = "visible";
 }
