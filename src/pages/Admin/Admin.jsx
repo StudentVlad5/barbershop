49265c550bfaf4993211eb9519bbf4657253884d
JuliaGolban <julia.j.shcherban@gmail.com>
2023-06-12 22:54:09 +0300

    fix admin page

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { SEO } from 'utils/SEO';
 import css from 'components/Admin/admin.module.scss';
+import { User } from 'components/Sections/User/User';
 
 const AdminPage = () => {
   return (
     <>
       <SEO title="Administration" description="Page Administration" />
+      <User />
       <section className={'admin' + ' ' + css.section} id="admin">
-        <div className={css.container}>
+        <div className={css.admin__container}>
           <div className={css['title-group']}>
-            <h2 className={css['section-title'] + ' ' + css['visually-hidden']}>
-              Administration
-            </h2>
+            <h2 className={css['section-title--size-s']}>Administration:</h2>
           </div>
           <ul className={css.admin__list + ' ' + css.list}>
             <li className={css.admin__item}>
