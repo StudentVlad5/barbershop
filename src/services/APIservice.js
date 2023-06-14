111bfd67716e45e00106cde5a462b0745d306272
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-14 20:56:15 +0300

    Access-Control

--- src/services/APIservice.js
+++ src/services/APIservice.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import PropTypes from 'prop-types';
+import axios from "axios";
+import PropTypes from "prop-types";
 
-const BASE_URL = 'https://drab-pear-gazelle-belt.cyclic.app/api';
+const BASE_URL = "https://drab-pear-gazelle-belt.cyclic.app/api";
 // const BASE_URL = 'http://localhost:3030/api';
 
 async function fetchData(pathParams) {
   const axiosInstance = axios.create({
     baseURL: `${BASE_URL}${pathParams}`,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-      'Access-Control-Expose-Headers': 'Content-Range',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 
@@ -20,37 +20,39 @@ async function fetchData(pathParams) {
 
 async function updateUserData(pathParams, body, file) {
   const formData = new FormData();
-  file && formData.set('avatar', file);
-  formData.append('email', body.email);
-  formData.append('birthday', body.birthday);
-  formData.append('location', body.location);
-  formData.append('password', body.password);
-  formData.append('phone', body.phone);
-  formData.append('role', body.role);
-  formData.append('userName', body.userName);
+  file && formData.set("avatar", file);
+  formData.append("email", body.email);
+  formData.append("birthday", body.birthday);
+  formData.append("location", body.location);
+  formData.append("password", body.password);
+  formData.append("phone", body.phone);
+  formData.append("role", body.role);
+  formData.append("userName", body.userName);
 
   return await axios.patch(`${BASE_URL}${pathParams}`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 }
 
 async function updateServiceData(pathParams, body) {
   const formData = new FormData();
-  formData.append('subject', body.subject);
-  formData.append('time', body.time);
-  formData.append('location', body.location);
-  formData.append('price', body.price);
-  formData.append('owner', body.owner);
+  formData.append("subject", body.subject);
+  formData.append("time", body.time);
+  formData.append("location", body.location);
+  formData.append("price", body.price);
+  formData.append("owner", body.owner);
 
   return await axios.patch(`${BASE_URL}${pathParams}`, formData, {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 }
@@ -65,28 +67,30 @@ async function createServiceData(pathParams, body) {
 
   return await axios.post(`${BASE_URL}${pathParams}`, formData, {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 }
 
 async function updateSpecialistData(pathParams, body) {
   const formData = new FormData();
-  formData.append('ownerText', body.ownerText);
-  formData.append('ownerColor', body.ownerColor);
-  formData.append('designation', body.designation);
-  formData.append('workDays', body.workDays);
-  formData.append('startHour', body.startHour);
-  formData.append('endHour', body.endHour);
-  formData.append('groupId', body.groupId);
+  formData.append("ownerText", body.ownerText);
+  formData.append("ownerColor", body.ownerColor);
+  formData.append("designation", body.designation);
+  formData.append("workDays", body.workDays);
+  formData.append("startHour", body.startHour);
+  formData.append("endHour", body.endHour);
+  formData.append("groupId", body.groupId);
 
   return await axios.patch(`${BASE_URL}${pathParams}`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 }
@@ -95,9 +99,10 @@ async function deleteData(pathParams) {
   const formData = new FormData();
   return axios.delete(`${BASE_URL}${pathParams}`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      "Access-Control-Expose-Headers": "Content-Range",
     },
   });
 }
